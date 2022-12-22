d33ab096e6ec57d531017def9095f6caa9c18c5c
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-12-21 22:26:21 -0300

    Repositorio inicializado para resolver o problema do Vai Um

--- src/calc.test.ts
+++ src/calc.test.ts
@@ -5,38 +5,9 @@ describe('funcionalidade soma', () => {
         //setup
         const calculadora = new Calculadora();
         //action
-        const resultado = calculadora.soma('1+1');
+        const resultado = calculadora.confereVaiUm(2,2);
         //expectation
-        expect (resultado).toBe(2);
+        expect (resultado).toBe(0);
     })
-
-    test('somar dois números', () => {
-        //setup
-        const sut = new Calculadora();
-        //action
-        const resultado = sut.soma('2+1');
-        //expectation
-        expect (resultado).toBe(3);
-    })
-
-    test('somar dois números', () => {
-        //setup
-        const sut = new Calculadora();
-        //action
-        const resultado = sut.soma('2+2');
-        //expectation
-        expect (resultado).toBe(4);
-    })
-
 })
 
-describe ('funcionalidade divisao', ()=> {
-    test('dividir dois numeros', () =>{
-       //setup
-       const sut = new Calculadora();
-       //action
-       const resultado = sut.divisao('2/2');
-       //expectation
-       expect (resultado).toBe(1); 
-    })
-})
--- src/test.ts
+++
@@ -1,4 +0,0 @@
-test("primeiro teste", () => {
-
-    expect(1+1).toBe(2);
-})
